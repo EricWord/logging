3ca117137163a2b4a74d5add8f4856589ba9596c
EricWord <2635863525@qq.com>
2019-06-24 17:07:13 +0800

    SpringBoot日志完成

--- src/test/java/cn/cncoders/logging/LoggingApplicationTests.java
+++ src/test/java/cn/cncoders/logging/LoggingApplicationTests.java
@@ -2,6 +2,8 @@ package cn.cncoders.logging;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LoggingApplicationTests {
 
+
+    //日志记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() {
+        //日志级别由低到高
+        logger.trace("这是trace日志.....");
+        logger.debug("这是debug日志....");
+        //SpringBoot默认是info级别
+        //可以在配置文件中调整相应的级别
+        logger.info("这是info日志....");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
+
     }
 
 }
